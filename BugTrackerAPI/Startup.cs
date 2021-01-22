3220efef5b5696d69fa5e6547dc6a786ba621c32
Steven Garcia <stevgarcia2015@gmail.com>
2021-01-21 21:43:21 -0800

    Update dependency injection and middleware service configuration

--- BugTrackerAPI/Startup.cs
+++ BugTrackerAPI/Startup.cs
@@ -3,7 +3,7 @@ using BugTrackerAPI.Data;
 using BugTrackerAPI.Entities;
 using BugTrackerAPI.Helpers;
 using BugTrackerAPI.Interfaces;
-using Microsoft.AspNetCore.Authentication.Cookies;
+using BugTrackerAPI.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -15,9 +15,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace BugTrackerAPI
@@ -37,6 +39,10 @@ namespace BugTrackerAPI
             services.AddIdentityCore<User>(opt =>
             {
                 opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequireDigit = false;
+                opt.User.RequireUniqueEmail = true;
+                opt.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+/ ";
             })
                 .AddRoles<Role>()
                 .AddRoleManager<RoleManager<Role>>()
@@ -44,15 +50,32 @@ namespace BugTrackerAPI
                 .AddRoleValidator<RoleValidator<Role>>()
                 .AddEntityFrameworkStores<BugTrackerDbContext>();
 
-            services.AddScoped<IUserRepository, UserRepository>();
-            services.AddScoped<IProjectRepository, ProjectRepository>();
-            services.AddScoped<ITicketRepository, TicketRepository>();
+            services.AddScoped<ITokenService, TokenService>();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
 
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
 
+            services.AddCors();
+
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options => Configuration.Bind("JwtSettings", options))
-                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options => Configuration.Bind("CookieSettings", options));
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"])),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                    };
+                });
+
+            services.AddAuthorization(opt =>
+            {
+                opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                opt.AddPolicy("Manager", policy => policy.RequireRole("Admin", "Project Manager"));
+                opt.AddPolicy("Member", policy => policy.RequireRole("Admin", "Project Manager", "Developer"));
+            });
 
             services.AddDbContext<BugTrackerDbContext>(options =>
             {
@@ -70,10 +93,16 @@ namespace BugTrackerAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            app.UseCors(policy => policy.AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .WithOrigins("http://localhost:3000"));
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>

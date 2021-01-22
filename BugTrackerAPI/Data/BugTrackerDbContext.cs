c4d599ef0d599a7b2c8c6f826c616a8e571600dd
Steven Garcia <stevgarcia2015@gmail.com>
2021-01-21 21:51:05 -0800

    Add db context

---
+++ BugTrackerAPI/Data/BugTrackerDbContext.cs
@@ -0,0 +1,57 @@
+﻿using BugTrackerAPI.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTrackerAPI.Data
+{
+    public class BugTrackerDbContext : IdentityDbContext<User, Role, int, 
+        IdentityUserClaim<int>, UserRole, IdentityUserLogin<int>,
+        IdentityRoleClaim<int>, IdentityUserToken<int>>
+    {
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<Ticket> Tickets { get; set; }
+
+        public BugTrackerDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>()
+                .HasMany(ur => ur.UserRoles)
+                .WithOne(u => u.User)
+                .HasForeignKey(ur => ur.UserId)
+                .IsRequired();
+
+            builder.Entity<Role>()
+                .HasMany(ur => ur.UserRoles)
+                .WithOne(r => r.Role)
+                .HasForeignKey(ur => ur.RoleId)
+                .IsRequired();
+
+            builder.Entity<Project>()
+                .HasMany(p => p.Tickets)
+                .WithOne(t => t.Project)
+                .HasForeignKey(t => t.ProjectId)
+                .IsRequired();
+
+            builder.Entity<User>()
+                .HasMany(u => u.Projects)
+                .WithMany(p => p.Members);
+
+            builder.Entity<User>()
+                .HasMany(u => u.Tickets)
+                .WithOne(t => t.User)
+                .HasForeignKey(t => t.UserId)
+                .IsRequired();
+        }
+    }
+}

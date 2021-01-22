40be7d8672fe1b02c77809f14a7fc7b34de5bdd1
Steven Garcia <stevgarcia2015@gmail.com>
2021-01-21 21:54:50 -0800

    Add migration files

---
+++ BugTrackerAPI/Migrations/20210121060727_AddUsersIdToProjectEntity.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BugTrackerAPI.Migrations
+{
+    public partial class AddUsersIdToProjectEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Projects",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Projects");
+        }
+    }
+}

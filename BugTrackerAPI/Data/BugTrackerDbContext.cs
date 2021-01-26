f8ab2b08e26f57b068aae122707c471296a96abc
Steven Garcia <stevgarcia2015@gmail.com>
2021-01-25 22:52:15 -0800

    Update entity relationships in Db context

--- BugTrackerAPI/Data/BugTrackerDbContext.cs
+++ BugTrackerAPI/Data/BugTrackerDbContext.cs
@@ -17,6 +17,12 @@ namespace BugTrackerAPI.Data
 
         public DbSet<Ticket> Tickets { get; set; }
 
+        public DbSet<ProjectMember> ProjectMembers { get; set; }
+
+        public DbSet<ProjectTicket> ProjectTickets { get; set;}
+
+        public DbSet<UserAssignedTicket> UserAssignedTickets { get; set; }
+
         public BugTrackerDbContext(DbContextOptions options) : base(options)
         {
         }
@@ -37,20 +43,51 @@ namespace BugTrackerAPI.Data
                 .HasForeignKey(ur => ur.RoleId)
                 .IsRequired();
 
-            builder.Entity<Project>()
-                .HasMany(p => p.Tickets)
-                .WithOne(t => t.Project)
-                .HasForeignKey(t => t.ProjectId)
+            builder.Entity<ProjectMember>()
+                .HasKey(pm => new { pm.UserId, pm.ProjectId });
+
+            builder.Entity<ProjectMember>()
+                .HasOne(pm => pm.User)
+                .WithMany(u => u.ProjectMembers)
+                .HasForeignKey(pm => pm.UserId)
                 .IsRequired();
 
-            builder.Entity<User>()
-                .HasMany(u => u.Projects)
-                .WithMany(p => p.Members);
+            builder.Entity<ProjectMember>()
+                .HasOne(pm => pm.Project)
+                .WithMany(p => p.ProjectMembers)
+                .HasForeignKey(pm => pm.ProjectId)
+                .IsRequired();
 
-            builder.Entity<User>()
-                .HasMany(u => u.Tickets)
-                .WithOne(t => t.User)
-                .HasForeignKey(t => t.UserId)
+            builder.Entity<ProjectTicket>()
+                .HasKey(pt => new { pt.ProjectId, pt.TicketId });
+
+            builder.Entity<ProjectTicket>()
+                .HasOne(pt => pt.Project)
+                .WithMany(p => p.ProjectTickets)
+                .HasForeignKey(pt => pt.ProjectId)
+                .IsRequired();
+
+            builder.Entity<ProjectTicket>()
+                .HasOne(pt => pt.Ticket)
+                .WithMany(t => t.ProjectTickets)
+                .HasForeignKey(pt => pt.TicketId)
+                .OnDelete(DeleteBehavior.NoAction)
+                .IsRequired();
+
+            builder.Entity<UserAssignedTicket>()
+                .HasKey(uat => new { uat.TicketId, uat.UserId });
+
+            builder.Entity<UserAssignedTicket>()
+                .HasOne(uat => uat.Ticket)
+                .WithMany(t => t.UserAssignedTickets)
+                .HasForeignKey(uat => uat.TicketId)
+                .IsRequired();
+
+            builder.Entity<UserAssignedTicket>()
+                .HasOne(uat => uat.User)
+                .WithMany(t => t.UserAssignedTickets)
+                .HasForeignKey(uat => uat.UserId)
+                .OnDelete(DeleteBehavior.NoAction)
                 .IsRequired();
         }
     }

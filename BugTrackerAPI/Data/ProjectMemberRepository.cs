d5fee046f354044012c7c6166bbdfa4ab3810395
Steven Garcia <stevgarcia2015@gmail.com>
2021-01-27 21:11:31 -0800

    Modify interface and repository for ProjectMember

--- BugTrackerAPI/Data/ProjectMemberRepository.cs
+++ BugTrackerAPI/Data/ProjectMemberRepository.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
+using BugTrackerAPI.DataTransferObjects;
 using BugTrackerAPI.Entities;
 using BugTrackerAPI.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace BugTrackerAPI.Data
 {
@@ -12,6 +14,36 @@ namespace BugTrackerAPI.Data
     {
         public ProjectMemberRepository(BugTrackerDbContext context) : base(context)
         {
+        }
+
+        public async Task<IEnumerable<ProjectMemberDto>> GetMembersInAProject(int projectId)
+        {
+            return await _context.Set<ProjectMember>()
+                .Where(pm => pm.ProjectId == projectId)
+                .Include("User")
+                .Select(pm => new ProjectMemberDto
+                {
+                    Id = pm.User.Id,
+                    UserName = pm.User.UserName,
+                    JobTitle = pm.User.JobTitle,
+                    ImageUrl = ""
+                })
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<BaseProjectDto>> GetProjectsForUser(int userId)
+        {
+            return await _context.Set<ProjectMember>()
+                .Where(m => m.UserId == userId)
+                .Include("Project")
+                .Select(pm => new BaseProjectDto
+                {
+                    Id = pm.ProjectId,
+		            Name = pm.Project.Name,
+		            Description = pm.Project.Description,
+		            DateCreated = pm.Project.DateCreated,
+                })
+                .ToListAsync();
         }
     }
 }
--- BugTrackerAPI/Interfaces/IProjectMemberRepository.cs
+++ BugTrackerAPI/Interfaces/IProjectMemberRepository.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
+using BugTrackerAPI.DataTransferObjects;
 using BugTrackerAPI.Entities;
 
 namespace BugTrackerAPI.Interfaces
 {
     public interface IProjectMemberRepository : IRepository<ProjectMember>
     {
+        Task<IEnumerable<ProjectMemberDto>> GetMembersInAProject(int projectId);
+        Task<IEnumerable<BaseProjectDto>> GetProjectsForUser(int userId);
     }
 }

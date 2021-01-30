353cbcd776a569b9df76950f8643584b69682bef
Steven Garcia <stevgarcia2015@gmail.com>
2021-01-29 17:41:33 -0800

    Add description and user properties to Ticket Data Transfer Object

--- BugTrackerAPI/DataTransferObjects/TicketDto.cs
+++ BugTrackerAPI/DataTransferObjects/TicketDto.cs
@@ -6,10 +6,13 @@ namespace BugTrackerAPI.DataTransferObjects
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public string Description { get; set; }
         public string Type { get; set; }
         public string Priority { get; set; }
         public string Status { get; set; }
         public DateTime DateCreated { get; set; }
         public int ProjectId { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
     }
 }

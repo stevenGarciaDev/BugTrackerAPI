fbcf1ca526ebd595c097287e321534371d728398
Steven Garcia <stevgarcia2015@gmail.com>
2021-01-29 17:44:28 -0800

    Implement IProjectTicketRepository

--- BugTrackerAPI/Data/ProjectTicketRepository.cs
+++ BugTrackerAPI/Data/ProjectTicketRepository.cs
@@ -1,5 +1,10 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BugTrackerAPI.DataTransferObjects;
 using BugTrackerAPI.Entities;
 using BugTrackerAPI.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace BugTrackerAPI.Data
 {
@@ -7,6 +12,25 @@ namespace BugTrackerAPI.Data
     {
         public ProjectTicketRepository(BugTrackerDbContext context) : base(context)
         {
+        }
+
+        public async Task<IEnumerable<TicketDto>> GetTicketsForProject(int projectId)
+        {
+            return await _context.Set<ProjectTicket>()
+                .Where(pt => pt.ProjectId == projectId)
+	            .Include(pt => pt.Ticket)
+	            .Select(pt => new TicketDto
+	            {
+		            Id = pt.Ticket.Id,
+		            Title = pt.Ticket.Title,
+		            Description = pt.Ticket.Description,
+		            Type = pt.Ticket.Type,
+		            Priority = pt.Ticket.Priority,
+		            Status = pt.Ticket.Status,
+		            DateCreated = pt.Ticket.DateCreated,
+                    ProjectId = pt.ProjectId,
+	            })
+	            .ToListAsync();
         }
     }
 }

d83d184d0c1a31145f1b4c262c936c4879b488f0
Steven Garcia <stevgarcia2015@gmail.com>
2021-01-29 17:43:49 -0800

    Implement ITicketRepository interface

--- BugTrackerAPI/Data/TicketRepository.cs
+++ BugTrackerAPI/Data/TicketRepository.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BugTrackerAPI.DataTransferObjects;
+using Microsoft.EntityFrameworkCore;
 
 namespace BugTrackerAPI.Data
 {
@@ -11,6 +13,69 @@ namespace BugTrackerAPI.Data
     {
         public TicketRepository(BugTrackerDbContext context) : base(context)
         {
+        }
+
+        public async Task<IEnumerable<TicketDto>> GetAllTicketsForAUser(int userId)
+        {
+            return await _context.Set<Ticket>()
+                .Where(t => t.UserId == userId)
+                .Include(t => t.User)
+                .Select(t => new TicketDto
+                {
+                    Id = t.Id,
+                    Title = t.Title,
+                    Description = t.Description,
+                    Type = t.Type,
+                    Priority = t.Priority,
+                    Status = t.Status,
+                    DateCreated = t.DateCreated,
+                    UserId = t.UserId,
+                    UserName = t.User.UserName,
+                    ProjectId = t.ProjectId,
+                })
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<TicketDto>> GetTicketsInAProject(int projectId)
+        {
+            return await _context.Set<Ticket>()
+                .Where(t => t.ProjectId == projectId)
+	            .Include(t => t.User)
+	            .Select(t => new TicketDto
+	            {
+                    Id = t.Id,
+                    Title = t.Title,
+                    Description = t.Description,
+                    Type = t.Type,
+                    Priority = t.Priority,
+                    Status = t.Status,
+                    DateCreated = t.DateCreated,
+                    UserId = t.UserId,
+                    UserName = t.User.UserName,
+                    ProjectId = t.ProjectId,
+                })
+                .ToListAsync();
+        }
+
+        public TicketDto GetTicketWithAssignedUser(int ticketId)
+        {
+            return _context.Set<Ticket>()
+                .Where(t => t.Id == ticketId)
+                .Include(t => t.User)
+                .Select(t => new TicketDto
+                {
+                    Id = t.Id,
+		            Title = t.Title,
+		            Description = t.Description,
+		            Type = t.Type,
+		            Priority = t.Priority,
+		            Status = t.Status,
+		            DateCreated = t.DateCreated,
+		            UserId = t.UserId,
+		            UserName = t.User.UserName,
+                    ProjectId = t.ProjectId,
+                })
+                .SingleOrDefault();
         }
     }
 }

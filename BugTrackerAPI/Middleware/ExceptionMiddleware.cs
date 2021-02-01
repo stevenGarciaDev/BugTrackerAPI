17cb334f10649e2b3c4f07c899efa240521a8b84
Steven Garcia <stevgarcia2015@gmail.com>
2021-02-01 13:58:48 -0800

    Update exception middleware for testing

--- BugTrackerAPI/Middleware/ExceptionMiddleware.cs
+++ BugTrackerAPI/Middleware/ExceptionMiddleware.cs
@@ -31,18 +31,22 @@ namespace BugTrackerAPI.Middleware
             catch (Exception ex)
             {
                 this.logger.LogError(ex, ex.Message);
-                    context.Response.ContentType = "application/json";
-                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
-                    var response = this.env.IsDevelopment()
-                        ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
-                        : new ApiException(context.Response.StatusCode, "Internal Server Error");
+                // var response = this.env.IsDevelopment()
+                //     ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
+                //     : new ApiException(context.Response.StatusCode, "Internal Server Error");
 
-                    var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+                var response = this.env.IsDevelopment()
+                    ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
+                    : new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString());
 
-                    var json = JsonSerializer.Serialize(response, options);
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
 
-                    await context.Response.WriteAsync(json);
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
             }
         }
     }

235ba0ba4d9fe8ba99af14f1a81b691f22eed5c7
Steven Garcia <stevgarcia2015@gmail.com>
2021-03-11 16:54:43 -0800

    Update exception middleware

--- BugTrackerAPI/Middleware/ExceptionMiddleware.cs
+++ BugTrackerAPI/Middleware/ExceptionMiddleware.cs
@@ -34,13 +34,9 @@ namespace BugTrackerAPI.Middleware
                 context.Response.ContentType = "application/json";
                 context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
-                // var response = this.env.IsDevelopment()
-                //     ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
-                //     : new ApiException(context.Response.StatusCode, "Internal Server Error");
-
                 var response = this.env.IsDevelopment()
                     ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
-                    : new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString());
+                    : new ApiException(context.Response.StatusCode, "Internal Server Error");
 
                 var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
 
